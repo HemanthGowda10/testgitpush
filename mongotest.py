f4cc9dbb915be1dbdacdccba4f966f767ee0613e
Hemanth Gowda <hg8655@gmail.com>
2022-08-11 20:14:44 +0530

    1st code

--- mongotest.py
+++ mongotest.py
@@ -3,14 +3,12 @@ import pymongo
 client = pymongo.MongoClient("mongodb+srv://hg8655:8655340812@cluster0.mwt706o.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
 
-print(db)
-
-d={
-    "name":"shudhanshu",
-    "email":"sudhanshu2",
-    "surname":"kumar"
+data = {
+    "name" : "sudhanshu",
+    "mail_id" : "sudhanshu@ineuron.ai",
+    "subject" : ["data scinece" , "big data " , "data analytics "]
 }
 
-db1 = client['mongotest']
-coll = db1['test']
-coll.insert_one(d )
+database=client['myinfo']
+collecion=database['sudh']
+collection.insert_one(data)
